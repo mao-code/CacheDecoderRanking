3a38f31aeaa98bcd2107c2b2a20d42c58aea27fb
MaoXun <a22908352@gmail.com>
2025-03-30 16:00:25 +0800

    modify padding logic and ensure grad_acc to 1

--- finetune/finetune.py
+++ finetune/finetune.py
@@ -379,8 +379,8 @@ if __name__ == "__main__":
     --prepared_data_sample_counts "650000,100000,150000,50000,20000" \
     --n_per_query 15 \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
-    --gradient_accumulation_steps 4 \
+    --per_device_train_batch_size 32 \
+    --gradient_accumulation_steps 1 \
     --lr 1e-5 \
     --weight_decay 0.01 \
     --sample_dev_percentage 0.1 \
